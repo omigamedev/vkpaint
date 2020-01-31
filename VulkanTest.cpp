f30833899dcce196641040ff07c254184707b737
omigamedev <omarator@gmail.com>
2020-01-31 20:22:31 +0100

    add uniform buffer

--- VulkanTest.cpp
+++ VulkanTest.cpp
@@ -11,6 +11,7 @@
 #define GLM_FORCE_RADIANS
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/euler_angles.hpp"
 
 #include "debug_message.h"
 
@@ -100,7 +101,11 @@ auto init_pipeline(const vk::UniqueDevice& dev, const vk::UniqueShaderModule& ve
     auto pipeline_blend = vk::PipelineColorBlendStateCreateInfo({}, false, vk::LogicOp::eCopy, 1, &pipeline_blend_state);
     auto pipeline_dyn = vk::PipelineDynamicStateCreateInfo();
 
-    auto pipeline_layout_info = vk::PipelineLayoutCreateInfo();
+    auto pipeline_layout_bind = vk::DescriptorSetLayoutBinding(0, vk::DescriptorType::eUniformBuffer,
+        1, vk::ShaderStageFlagBits::eVertex, nullptr);
+    auto pipeline_layout_descr_info = vk::DescriptorSetLayoutCreateInfo({}, 1, &pipeline_layout_bind);
+    auto pipeline_layout_descr = dev->createDescriptorSetLayoutUnique(pipeline_layout_descr_info);
+    auto pipeline_layout_info = vk::PipelineLayoutCreateInfo({}, 1, &pipeline_layout_descr.get(), 0, nullptr);
     auto pipeline_layout = dev->createPipelineLayoutUnique(pipeline_layout_info);
 
     auto pipeline_renderpass_fb = vk::AttachmentDescription({}, vk::Format::eB8G8R8A8Unorm,
@@ -127,7 +132,8 @@ auto init_pipeline(const vk::UniqueDevice& dev, const vk::UniqueShaderModule& ve
         *pipeline_renderpass, 0,
         nullptr, 0);
     vk::UniquePipeline pipeline = dev->createGraphicsPipelineUnique(nullptr, pipeline_info);
-    return std::tuple(std::move(pipeline), std::move(pipeline_renderpass));
+    return std::tuple(std::move(pipeline), std::move(pipeline_renderpass), 
+        std::move(pipeline_layout), std::move(pipeline_layout_descr));
 }
 
 std::tuple<vk::UniqueImage, vk::UniqueImageView, vk::UniqueDeviceMemory> create_depth(const vk::PhysicalDevice& pd, vk::Device const& dev, int width, int height)
@@ -179,30 +185,35 @@ auto create_triangle(const vk::PhysicalDevice& pd, const vk::UniqueDevice& dev)
 
 auto create_uniforms(const vk::PhysicalDevice& pd, const vk::UniqueDevice& dev)
 {
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(-5.0f, 3.0f, -10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
-    // vulkan clip space has inverted y and half z !
-    glm::mat4 clip = glm::mat4(
-        1.0f, 0.0f, 0.0f, 0.0f, 
-        0.0f,-1.0f, 0.0f, 0.0f, 
-        0.0f, 0.0f, 0.5f, 0.0f, 
-        0.0f, 0.0f, 0.5f, 1.0f);
-    glm::mat4 mvpc = clip * projection * view * model;
-
-    auto uniform_buffer_info = vk::BufferCreateInfo({}, sizeof(mvpc), vk::BufferUsageFlagBits::eUniformBuffer);
+    auto uniform_buffer_info = vk::BufferCreateInfo({}, sizeof(glm::mat4), vk::BufferUsageFlagBits::eUniformBuffer);
     auto uniform_buffer = dev->createBufferUnique(uniform_buffer_info);
     auto uniform_mem_req = dev->getBufferMemoryRequirements(*uniform_buffer);
     auto uniform_mem_idx = find_memory(pd, uniform_mem_req, 
         vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
-    auto uniform_mem = dev->allocateMemoryUnique({ uniform_mem_req.size, uniform_mem_idx });
+    auto uniform_mem = dev->allocateMemoryUnique({ uniform_mem_req.size, (uint32_t)uniform_mem_idx });
     dev->bindBufferMemory(*uniform_buffer, *uniform_mem, 0);
-    if (auto uniform_map = static_cast<glm::mat4*>(dev->mapMemory(*uniform_mem, 0, uniform_mem_req.size)))
+    return std::tuple(std::move(uniform_buffer), std::move(uniform_mem));
+}
+
+void update_uniforms(const vk::UniqueDevice& dev, const vk::UniqueDeviceMemory& uniform_mem)
+{
+    static float theta = 0;
+    theta += glm::radians(1.f);
+    glm::mat4 model = glm::eulerAngleZ(theta);
+    glm::mat4 view = glm::lookAt(glm::vec3(-5.0f, 3.0f, -10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+    // vulkan clip space has inverted y and half z !
+    glm::mat4 clip = glm::mat4(
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, -1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.5f, 0.0f,
+        0.0f, 0.0f, 0.5f, 1.0f);
+    glm::mat4 mvpc = clip * projection * view * model;
+    if (auto uniform_map = static_cast<glm::mat4*>(dev->mapMemory(*uniform_mem, 0, VK_WHOLE_SIZE)))
     {
         std::copy_n(&mvpc, 1, uniform_map);
         dev->unmapMemory(*uniform_mem);
     }
-    return std::tuple(std::move(uniform_buffer), std::move(uniform_mem));
 }
 
 int main()
@@ -271,10 +282,14 @@ int main()
 
                     auto vert_module = load_shader(*dev, "shader.vert.spv");
                     auto frag_module = load_shader(*dev, "shader.frag.spv");
-                    auto [pipeline, renderpass] = init_pipeline(dev, vert_module, frag_module, surface_caps.currentExtent);
+                    auto [pipeline, renderpass, layout, layout_descr] = init_pipeline(dev, 
+                        vert_module, frag_module, surface_caps.currentExtent);
 
-                    auto pool_info = vk::CommandPoolCreateInfo({}, idx);
-                    auto cmd_pool = dev->createCommandPoolUnique(pool_info);
+                    auto cmd_pool_info = vk::CommandPoolCreateInfo({}, idx);
+                    auto cmd_pool = dev->createCommandPoolUnique(cmd_pool_info);
+                    auto descr_pool_size = vk::DescriptorPoolSize(vk::DescriptorType::eUniformBuffer, 1);
+                    auto descr_pool_info = vk::DescriptorPoolCreateInfo({}, 2, 1, &descr_pool_size);
+                    auto descr_pool = dev->createDescriptorPoolUnique(descr_pool_info);
 
                     auto [triangle_buffer, triangle_mem] = create_triangle(pd, dev);
                     auto [uniform_buffer, uniform_mem] = create_uniforms(pd, dev);
@@ -283,6 +298,7 @@ int main()
                     std::vector<vk::UniqueImageView> sc_image_views(sc_images.size());
                     std::vector<vk::UniqueFramebuffer> framebuffers(sc_images.size());
                     std::vector<vk::UniqueCommandBuffer> cmd(sc_images.size());
+                    std::vector<vk::UniqueDescriptorSet> descr(sc_images.size());
                     for (size_t image_index = 0; image_index < sc_images.size(); image_index++)
                     {
                         auto view_info = vk::ImageViewCreateInfo({}, sc_images[image_index],
@@ -298,6 +314,13 @@ int main()
                         auto cmd_info = vk::CommandBufferAllocateInfo(*cmd_pool, vk::CommandBufferLevel::ePrimary, 1);
                         cmd[image_index] = std::move(dev->allocateCommandBuffersUnique(cmd_info).front());
 
+                        auto descr_info = vk::DescriptorSetAllocateInfo(*descr_pool, 1, &layout_descr.get());
+                        descr[image_index] = std::move(dev->allocateDescriptorSetsUnique(descr_info).front());
+                        auto descr_buffer_info = vk::DescriptorBufferInfo(*uniform_buffer, 0, VK_WHOLE_SIZE);
+                        auto descr_write = vk::WriteDescriptorSet(*descr[image_index], 0, 0, 1, 
+                            vk::DescriptorType::eUniformBuffer, nullptr, &descr_buffer_info, nullptr);
+                        dev->updateDescriptorSets(descr_write, nullptr);
+
                         vk::ClearValue clearColor(std::array<float, 4>{ 0.3f, 0.0f, 0.0f, 1.0f });
                         auto begin_info = vk::RenderPassBeginInfo(*renderpass, *framebuffers[image_index],
                             vk::Rect2D({ 0, 0 }, surface_caps.currentExtent), 1, &clearColor);
@@ -306,6 +329,8 @@ int main()
                         cmd[image_index]->beginRenderPass(begin_info, vk::SubpassContents::eInline);
                         cmd[image_index]->bindPipeline(vk::PipelineBindPoint::eGraphics, *pipeline);
                         cmd[image_index]->bindVertexBuffers(0, *triangle_buffer, { 0 });
+                        cmd[image_index]->bindDescriptorSets(vk::PipelineBindPoint::eGraphics,
+                            *layout, 0, *descr[image_index], nullptr);
                         cmd[image_index]->draw(3, 1, 0, 0);
                         cmd[image_index]->endRenderPass();
                         cmd[image_index]->end();
@@ -321,6 +346,8 @@ int main()
                     {
                         TranslateMessage(&msg);
                         DispatchMessage(&msg);
+
+                        update_uniforms(dev, uniform_mem);
 
                         auto swapchain_sem = dev->createSemaphoreUnique(vk::SemaphoreCreateInfo());
                         auto swapchain_idx = dev->acquireNextImageKHR(*swapchain, UINT64_MAX, *swapchain_sem, nullptr);
--- shader.vert
+++ shader.vert
@@ -1,12 +1,13 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
+layout(binding = 0) uniform values { mat4 mvp; } ubo;
 layout(location = 0) in vec2 vpos;
 layout(location = 1) in vec3 vcol;
 layout(location = 0) out vec3 fcol;
 
 void main()
 {
-    gl_Position = vec4(vpos, 0.0, 1.0);
+    gl_Position = ubo.mvp * vec4(vpos, 0.0, 1.0);
     fcol = vcol;
 }
